31b7d77a3ba415d59c12923c1a90ef0b25aac660
martcpp <mordecaiemmanueletukudo@gmail.com>
2024-05-30 16:54:44 +0100

    added some file to .gitinore

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,78 @@
-use anyhow::Ok;
-use tokio::time::sleep;
-use std::time::Duration;
+use futures::{SinkExt,StreamExt};
+use tokio::net:: {TcpListener,TcpStream};
+// use tokio::{net::{TcpListener,TcpStream}, sync::broadcast};
+use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
+use tokio::sync::broadcast::{self, Sender};
+use tokio::select;
+use client::random_name;
+
+mod client;
 
 
-async fn counter_to(num:u32) {
+// use tokio::{net::{TcpListener, TcpStream}, sync::broadcast::{self, Sender}};
 
-    for count in 1..=num {
-       sleep(Duration::from_millis(100)).await;
-        println!("{}",count);
-      }
+const HELP:&str = include_str!("help.txt");
+//const ADDRESS:&str = "192.168.0.103:42063";
 
+
+async fn handle_connection(mut conn: TcpStream, txp:Sender<String>)-> anyhow::Result<()> {
+
+    let (reader,writer) = conn.split();
+    let mut stream: FramedRead<tokio::net::tcp::ReadHalf, LinesCodec> = FramedRead::new(reader,LinesCodec::new());
+    let mut sink = FramedWrite::new(writer,LinesCodec::new());
+
+    let name = random_name();
+    sink.send(HELP).await?;
+    sink.send(format!("Welcome {name}!")).await?;
+
+    // let (txp,rx) = broadcast::channel::<String>(32);
+    let mut rx = txp.subscribe();
+
+    loop{
+        select!{
+            user_msg = stream.next() => {
+                
+                let user_msg = match user_msg{
+                    Some(msg) => msg?,
+                    None => break,
+                };
+                
+                if user_msg.starts_with("/help"){
+                    sink.send(HELP).await?;
+                }
+                else if user_msg.starts_with("/quit"){
+                    break;
+                }
+
+                else{
+                    txp.send(format!("{name}: {user_msg}"))?;
+                }
+
+            },
+
+
+            peer_msg = rx.recv() => {
+                sink.send(peer_msg?).await?;
+            },
+        }
+    }
+
+  Ok(())
 
 }
 
 
 
 
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let server = TcpListener::bind("192.168.0.103:42063").await?;
+    //let server = TcpListener::bind("192.168.0.178:42063").await?;
+    let (txp,_) = broadcast::channel::<String>(32);
+    loop{
+        let (tcp,_) = server.accept().await?;
+        tokio::spawn(handle_connection(tcp, txp.clone()));
 
-#[tokio::main]  
-
-async fn main() -> anyhow::Result<()>{
-  let couter_to_10 = counter_to(10);
-  tokio::pin!(couter_to_10);
-
-  tokio::select! {
-    _ = counter_to(5) => {
-      println!("Counter 3 finished")
-    },
-    _ = &mut couter_to_10 => { 
-      println!("Counter 5 finished")
-    },
-  };
-  println!("stop counting");
-    println!("jk, keep counting");
-    couter_to_10.await; 
-    println!("finished counting to 10");
-
-    Ok(())
+    }
 }
+
